296d993419f335fbabf12526ff973cff3a7c1d0d
Abraham <abeis@msn.com>
2019-01-09 21:43:15 -0600

    added input changle handles and sending info to parent

--- src/App.js
+++ src/App.js
@@ -8,15 +8,20 @@ class App extends Component {
     totalCalories: 0
   }
 
-  handleChange(test)  {
-    console.log(test);
+  handleChange =  (test, active) =>   {
+    if(active){
+      this.setState({ totalCalories: this.state.totalCalories - test });
+    }else{
+      this.setState({ totalCalories: this.state.totalCalories + test });
+    }
+   
   }
 
   render() {
     const foodList =  [{"food": "Chicken brest", "calories": 250  }, {"food": "Bacon Thick Cut", "calories": 56  } ,{"food": "Lettus Heart", "calories": 5  }];
     return (
       <div className="App">
-        {foodList.map(food => <FoodItem food={food.food} calories={food.calories} testProp={() => this.handleChange}/> )}
+        {foodList.map(food => <FoodItem food={food.food} calories={food.calories} testProp={this.handleChange}/> )}
         <div> TEST VALUE = {this.state.totalCalories} </div>
       </div>
     );
--- src/componets/FoodItem.js
+++ src/componets/FoodItem.js
@@ -5,11 +5,24 @@ class FoodItem extends React.Component{
         state = {
             quanity: 2,
             peopleShareing: 2,
-            totalCalories: 0
-            
+            totalCalories: 0,
+            active: false
         }
+        handleChange(event) {
+            this.setState({ [event.target.name]: event.target.value });
+          }
+
          componentDidMount(){
+            this.updateTotalCalories();
+         }
+
+         updateTotalCalories(){
             this.setState((state) => ({ totalCalories: (this.props.calories * this.state.quanity)/ this.state.peopleShareing}));
+         }
+
+         sendTotalCalories = () => {
+            this.setState({active: !this.state.active})
+            this.props.testProp(this.state.totalCalories, this.state.active)
          }
     
     
@@ -17,11 +30,11 @@ class FoodItem extends React.Component{
     render(){
         return(
             <div>
-                <input onClick={this.props.testProp("123")} type="checkbox" />
+                <input onClick={() => this.sendTotalCalories() } type="checkbox" />
                 <label> { this.props.food } </label>
                 <label> {this.props.calories} </label>
-                <label> Quanity <input  value={this.state.quanity} />  </label>
-                <label> People <input  value={this.state.peopleShareing} />  </label>
+                <label> Quanity <input name="quanity"  value={this.state.quanity} />  </label>
+                <label> People <input name="peopleShareing"  value={this.state.peopleShareing} />  </label>
                 <label> Total: {this.state.totalCalories} </label>
             </div>
         );
